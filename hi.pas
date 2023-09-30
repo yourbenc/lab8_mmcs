86d6446e6fdef6e59e5910bca3d265ebc9b49d3d
Петров Илья <comeonaye@mail.ru>
2023-09-30 08:44:53 +0300

    Крупное обновление

--- hi.pas
+++ hi.pas
@@ -1,3 +1,8 @@
 ﻿begin
-  print('Hello world')
+  var time := readinteger('');
+  assert((time>=0) and (time<=23));
+  if time in 4..10: print('Доброе утро, мир!');
+  if time in 11..16: print('Добрый день, мир!');
+  if time in 17..22: print('Добрый вечер, мир!');
+  else print('Доброй ночи, мир!')
 end.
