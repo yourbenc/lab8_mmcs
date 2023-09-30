84313b75d89751ae29b9d4a792b529f8bdacde81
Петров Илья <comeonaye@mail.ru>
2023-09-30 09:01:20 +0300

    new feature

---
+++ time.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var year:= readinteger('Введите год:');
+  assert(year>0);
+  if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then
+    print('Год високосный')
+  else print('Год невисокосный')
+end.
