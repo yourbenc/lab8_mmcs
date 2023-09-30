043adf25c6cd41f8798598ae8d8f3cb39c1e6d6e
Петров Илья <comeonaye@mail.ru>
2023-09-30 08:47:53 +0300

    major bug fixes

--- hi.pas
+++ hi.pas
@@ -1,8 +1,8 @@
 ﻿begin
-  var time := readinteger('');
+  var time := readinteger('Введите время:');
   assert((time>=0) and (time<=23));
-  if time in 4..10: print('Доброе утро, мир!');
-  if time in 11..16: print('Добрый день, мир!');
-  if time in 17..22: print('Добрый вечер, мир!');
+  if time in 4..10 then print('Доброе утро, мир!')
+  else if time in 11..16 then print('Добрый день, мир!')
+  else if time in 17..22 then print('Добрый вечер, мир!')
   else print('Доброй ночи, мир!')
 end.
