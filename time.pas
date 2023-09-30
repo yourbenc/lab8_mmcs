355a475e388eb5ba98b6ae44a8474d4314de096e
Петров Илья <comeonaye@mail.ru>
2023-09-30 09:07:55 +0300

    new dlc

--- time.pas
+++ time.pas
@@ -2,6 +2,6 @@
   var year:= readinteger('Введите год:');
   assert(year>0);
   if (year mod 4 = 0) and (year mod 100 <> 0) or (year mod 400 = 0) then
-    print('Год високосный')
-  else print('Год невисокосный')
+    print('В этом году 366 дней')
+  else print('В этом году 365 дней')
 end.
---
+++ timer.pas
@@ -0,0 +1,6 @@
+﻿begin
+  var (hours,minutes,seconds) := readinteger3('Введите часы, минуты и секунды');
+  assert((hours>=0)and(minutes>=0)and(seconds>=0));
+  var res := hours * 3600 + minutes*60 + seconds;;
+  print($'Это {res} секунд')
+end.
